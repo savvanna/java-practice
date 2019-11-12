a7a6d3888d2e2e009bb1efad0b67c39ae7d36bb0
akoltovich <alexeyugol@gmail.com>
2019-11-12 19:27:45 +0300

    in progress task 'Time'

---
+++ src/com/rakovets/course/challenge/classes/Time.java
@@ -0,0 +1,29 @@
+package com.rakovets.course.challenge.classes;
+
+public class Time {
+    int hours;
+    int minutes;
+    int seconds;
+
+    {
+        hours = 0;
+        minutes = 0;
+        seconds = 0;
+    }
+
+    Time(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    Time( int totalSeconds) {
+        this.hours =
+        this.minutes =
+        this.seconds = 
+    }
+    void displayInfo() {
+        System.out.printf(": ");
+    }
+}
+
---
+++ src/com/rakovets/course/challenge/classes/TimeTests.java
@@ -0,0 +1,8 @@
+package com.rakovets.course.challenge.classes;
+
+public class TimeTests {
+    public static void main(String[] args) {
+            Time lessonTime = new Time(14, 14, 14);
+            lessonTime.displayInfo();
+    }
+}
