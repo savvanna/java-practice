93e73797650821f8c858d5baef1b7cf0b3d27b82
savvanna <56040474+savvanna@users.noreply.github.com>
2019-12-17 19:30:43 +0300

    task1-7

--- src/com/rakovets/course/practice/module2/Task03.java
+++ src/com/rakovets/course/practice/module2/Task03.java
@@ -8,18 +8,21 @@ package com.rakovets.course.practice.module2;
  * @param penaltyPointSecondPlayer - штрафные очки второго игрока
  */
 public class Task03 {
-	public static void main(String[] args) {
-		//FIXME
-		// Переменные, которые можно изменять для проверки различных вариантов входных параметров
-		int penaltyPointFirstPlayer = 1;
-		int penaltyPointSecondPlayer = 2;
+    public static void main(String[] args) {
+        //FIXME
+        // Переменные, которые можно изменять для проверки различных вариантов входных параметров
+        int penaltyPointFirstPlayer = 1;
+        int penaltyPointSecondPlayer = 2;
 
-		// Код необходимый для тестирования, не изменять
-		penaltyPointFirstPlayer = (args.length == 0) ? penaltyPointFirstPlayer : Integer.parseInt(args[0]);
-		penaltyPointSecondPlayer = (args.length == 0) ? penaltyPointSecondPlayer : Integer.parseInt(args[1]);
+        // Код необходимый для тестирования, не изменять
+        penaltyPointFirstPlayer = (args.length == 0) ? penaltyPointFirstPlayer : Integer.parseInt(args[0]);
+        penaltyPointSecondPlayer = (args.length == 0) ? penaltyPointSecondPlayer : Integer.parseInt(args[1]);
 
-		//TODO
-		// Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
-		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
-	}
+        //TODO
+        // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
+        // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+
+        int c = penaltyPointSecondPlayer - penaltyPointFirstPlayer;
+        System.out.println(Math.min(penaltyPointFirstPlayer,penaltyPointSecondPlayer));
+    }
 }
--- src/com/rakovets/course/practice/module2/Task06.java
+++ src/com/rakovets/course/practice/module2/Task06.java
@@ -9,18 +9,19 @@ package com.rakovets.course.practice.module2;
  * @param annualPercentage - ежегодный простой процент, т.е. ежегодный процент высчитывается от первоначальной суммы
  */
 public class Task06 {
-	public static void main(String[] args) {
-		//FIXME
-		// Переменные, которые можно изменять для проверки различных вариантов входных параметров
-		int deposit = 1500;
-		int annualPercentage = 3;
+    public static void main(String[] args) {
+        //FIXME
+        // Переменные, которые можно изменять для проверки различных вариантов входных параметров
+        int deposit = 1500;
+        int annualPercentage = 3;
 
-		// Код необходимый для тестирования, не изменять
-		deposit = (args.length == 0) ? deposit : Integer.parseInt(args[0]);
-		annualPercentage = (args.length == 0) ? annualPercentage : Integer.parseInt(args[1]);
+        // Код необходимый для тестирования, не изменять
+        deposit = (args.length == 0) ? deposit : Integer.parseInt(args[0]);
+        annualPercentage = (args.length == 0) ? annualPercentage : Integer.parseInt(args[1]);
 
-		//TODO
-		// Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
-		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
-	}
+        //TODO
+        // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
+        // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+        System.out.println(1.0 *(deposit / 100 * annualPercentage) * 5 + deposit);
+    }
 }
